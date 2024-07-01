4bb87b85bf8828e9f61a304dd0e549c949692ef6
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2024-07-01 21:16:28 +0200

    gui correction for all imgs

--- gui.py
+++ gui.py
@@ -2,10 +2,11 @@ import gradio as gr
 from classification import classify
 
 input = gr.components.Image(label="Load Your Picture")
-outputRes18 = gr.components.Label(label='Resnet18 Prediction')
+outputCNN = gr.components.Label(label='CNN Prediction')
+outputVGG16 = gr.components.Label(label='VGG16 Prediction')
 outputRes50 = gr.components.Label(label='Resnet50 Prediction')
 
-output = [outputRes18, outputRes50]
+output = [outputVGG16, outputRes50]
 
 
 demo = gr.Interface(
