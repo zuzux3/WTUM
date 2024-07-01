b697021161036593424efca58f391f3d7b6f0c01
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2024-07-01 21:37:09 +0200

    edit classification

--- classification.py
+++ classification.py
@@ -3,38 +3,38 @@ import torch.nn as nn
 from preprocessing import imgPreprocess
 from torchvision import models
 
-def loadModelWeights(model, checkpointPath, device):
-    checkpoint = torch.load(checkpointPath, map_location=torch.device(device))
-    model.load_state_dict(checkpoint['stateDict'])
-    model.eval()
-    return model
-
 def classify(img):
     tensor = imgPreprocess(img)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
-    resnet18 = models.resnet18(weights=None)
-    resnet18 = loadModelWeights(resnet18, 'resnet18.pth', device)
-    
-    resnet50 = models.resnet18(weights=None)
-    resnet50 = loadModelWeights(resnet50, 'resnet50.pth', device)
-    
-    
-    model50 = models.resnet50(weights=None)
-    with torch.no_grad():
-        tensor = tensor.to(device)
-        outputRes18 = resnet18(tensor)
-        
-    _, predictedRes18 = torch.max(outputRes18, 1)
+    vgg16 = models.vgg16(weights=None)
+    num_ftrs = vgg16.classifier[6].in_features
+    vgg16.classifier[6] = nn.Linear(num_ftrs, 2)
+    checkpointVGG = torch.load('VGG16_model.pth', map_location=device)
+    vgg16.load_state_dict(checkpointVGG['model_state_dict'])
+    vgg16.to(device)
+    vgg16.eval()
     
     with torch.no_grad():
         tensor = tensor.to(device)
-        outputRes50 = resnet50(tensor)
+        outputVGG = vgg16(tensor)
+    _, predictedVGG = torch.max(outputVGG, 1)
         
-    _, predictedRes50 = torch.max(outputRes50, 1)
     
-    predictedRes18 = 'Not Pizza' if predictedRes18.item() == 0 else 'Pizza'
-    predictedRes50 = 'Not Pizza' if predictedRes50.item() == 0 else 'Pizza'
+    resnet50 = models.resnet50(weights=None)
+    resnet50.fc = nn.Linear(resnet50.fc.in_features, 2)
+    checkpointResnet = torch.load('ResNet50_model.pth', map_location=device)
+    resnet50.load_state_dict(checkpointResnet['model_state_dict'])
+    resnet50.to(device)
+    resnet50.eval()
     
-    return predictedRes18, predictedRes50
-     
+    with torch.no_grad():
+        tensor = tensor.to(device)
+        outputResnet = resnet50(tensor)
+    _, predictedResnet = torch.max(outputResnet, 1)
+    
+    predictionVGG = 'Pizza' if predictedVGG.item() == 1 else 'Not Pizza'
+    predictionResnet = 'Pizza' if predictedResnet.item() == 1 else 'Not Pizza'
+    
+    return predictionVGG, predictionResnet
+    
--- gui.py
+++ gui.py
@@ -2,7 +2,6 @@ import gradio as gr
 from classification import classify
 
 input = gr.components.Image(label="Load Your Picture")
-outputCNN = gr.components.Label(label='CNN Prediction')
 outputVGG16 = gr.components.Label(label='VGG16 Prediction')
 outputRes50 = gr.components.Label(label='Resnet50 Prediction')
 
